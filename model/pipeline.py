7e26f6dbd122f7cb0a567e02ea464ccfec9b08e7
IrinaProkofieva <athos_holmes@mail.ru>
2022-10-06 21:52:51 +0400

    Reformat code, print out the result image

--- model/pipeline.py
+++ model/pipeline.py
@@ -1,91 +1,89 @@
-import shutil
-from os import listdir, path
-
-import easyocr
-import numpy as np
-import torch
-from matplotlib import pyplot as plt
-
 import sys
+from os import path
+import cv2
+import torch
+from model.utils import translator, printer, predictor
 
 # Имитация подключения к БД с разрешенными номерами и данными о машине и водителе
 database = [
     {'number': 'с555вм',
      'region': 777,
-     'name': 'Круз, Том Томасович',
-     'company': 'ООО \'Миссия невыполнима\'',
+     'name': 'Cruise, Tom',
+     'company': 'OOO \'Mission impossible\'',
      'photo': 'https://pentad.ru/wp-content/uploads/2019/08/Tom-Kruz-v-filme-Dzhek-Richer.jpg'},
     {'number': 'н001ах',
      'region': 777,
-     'name': 'Питт, Уильямович Бред',
-     'company': 'ООО \'Бойцовский клуб\'',
+     'name': 'Pitt, Brad',
+     'company': 'OOO \'Fighting club\'',
      'photo': 'https://www.film.ru/sites/default/files/filefield_paths/leonardo-dicaprio-and-brad-pitt-are-seen-the'
               '-set-of-once-news-photo-1594220917.jpg'}
 ]
-
-
-# Функция первичного распознавания номера
-def predict(model, img_name):
-    results = model(img_name)  # находим номер на изображении
-    results.print()
-    plt.imshow(np.squeeze(results.render()))
-    plt.show()
-
-    save_path = './runs/detect/' + img_name.rsplit('/', 1)[-1].split('.')[0]
-    added_path = 'crops/plates'
-    shutil.rmtree(save_path, ignore_errors=True)
-    # вырезаем картинки с автомобильными номерами и сохраняем
-    list(map(lambda x: x['im'],
-             results.crop(save=True, save_dir=save_path, exist_ok=True)))
-    files = listdir('/'.join([save_path, added_path]))
-
-    # Подключаем модель easyOCR для распознавания символов на изображении
-    reader = easyocr.Reader(['ru'])
-    result = []
-    # Каждый найденный номер переводим в текст
-    for f in files:
-        result.append(reader.readtext('/'.join([save_path, added_path, f]),
-                                      allowlist='0123456789АаВвЕеКкМмНнОоРрСсТтУуХх',
-                                      detail=0, paragraph=True, min_size=60)[0])
-    return result
-
-
-# Функция, корректирующая распознанный номер
-def correct_plate_num(plate):
-    plate = plate.lower()  # все к нижнему регистру
-    # Из-за путаницы 0 и О явно указываем, где должны быть цифры, а где - буквы
-    part1 = plate[:1].replace('0', 'о')
-    part2 = plate[1:4].replace('о', '0')
-    part3 = plate[4:6].replace('0', 'о')
-    part4 = plate[6:].replace('o', '0')
-    return part1 + part2 + part3 + part4
 
 
 # Функция поиска номера в БД
 def get_data(plate_num, plate_code):
     result = list(filter(lambda x: x["number"] == plate_num and x["region"] == plate_code, database))
     if len(result) == 0:
-        raise Exception('Проезд запрещен:' + plate_num + str(plate_code))
+        return None
     return result[0]
+
+
+# Отрисовка результата
+def print_result(img_url, data, num):
+    # Изначальное изображение в качестве фона
+    image = printer.read_image(img_url)
+    # Затемнить фон
+    image = printer.darkening(image, 80)
+    # Изменить размер пропорционально с высотой 600
+    image = printer.resize(image, 600)
+
+    # Помещаем текст с распознанным номером
+    image = printer.set_text(image, translator.translate(num, translator.ru_eng).upper(), (50, 50), printer.white_color)
+    # Если данных о водителе нет в базе, выводим сообщение, что доступ ограничен. Иначе выводим данные и фото водителя
+    if len(data) == 0:
+        image = printer.set_text(image, 'Access denied', (50, 100), printer.red_color)
+    else:
+        image = printer.set_text(image, data[0]['name'], (50, 100), printer.white_color)
+        image = printer.set_text(image, data[0]['company'], (50, 150), printer.white_color)
+
+        # Получаем изображение водителя
+        image2 = printer.read_image(data[0]['photo'])
+        # Приводим фото к нужному нам размеру
+        image2 = printer.resize(image2, 200)
+        # Помещаем фото водителя на выводимое изображение
+        image = printer.set_image(image, image2, (50, 200))
+
+    # Выводим изображение
+    cv2.imshow('Result', image)
+
+    if cv2.waitKey() & 0xff == 27:
+        quit()
 
 
 # Основной сценарий работы с изображением
 def pipeline(model, img_name):
-    preds = predict(model, img_name)
+    # Получаем первичное предсказание номеров
+    preds = predictor.predict(model, img_name)
     cars_data = []
+    # Каждый найденный номер корректируем и ищем в базе
     for pred in preds:
-        corrected = correct_plate_num(pred)
+        corrected = predictor.correct_plate_num(pred)
         try:
-            cars_data.append(get_data(corrected[:6], int(corrected[6:])))
+            data = get_data(corrected[:6], int(corrected[6:]))
+            if data is not None:
+                cars_data.append(data)
         except Exception as e:
-            print(e)
-    return cars_data
+            print(corrected, e)
+    print_result(img_name, cars_data, corrected)
+    return cars_data, corrected
 
 
+# Путь до весов, которые будем использовать
 weights_path = '../yolov5/runs/train/exp/weights/last.pt'
 if not path.exists(weights_path):
     weights_path = '../last.pt'
 
 # Берем обученную нами модель
 mdl = torch.hub.load('ultralytics/yolov5', 'custom', path=weights_path, force_reload=True)
-print(pipeline(mdl, sys.argv[1]))
+img_path = sys.argv[1]
+pipeline(mdl, img_path)
--- model/utils/test.py
+++ model/utils/test.py
@@ -1,89 +1,29 @@
 import pandas as pd
-
 import torch
-
 from os import listdir
 import json
-import shutil
-import easyocr
 import fastwer
-
-
-# Функция первичного распознавания номера
-def predict(model, img_name):
-    results = model(img_name)  # находим номер на изображении
-    results.render()
-
-    save_path = './runs/detect/' + img_name.rsplit('/', 1)[-1].split('.')[0]
-    added_path = 'crops/plates'
-    shutil.rmtree(save_path, ignore_errors=True)
-    # вырезаем картинки с автомобильными номерами и сохраняем
-    list(map(lambda x: x['im'],
-             results.crop(save=True, save_dir=save_path, exist_ok=True)))
-    files = []
-    try:
-        files = listdir('/'.join([save_path, added_path]))
-    except Exception as e:
-        return []
-
-    # Подключаем модель easyOCR для распознавания символов на изображении
-    reader = easyocr.Reader(['ru'])
-    result = []
-    # Каждый найденный номер переводим в текст
-    for f in files:
-        try:
-            result.append(reader.readtext('/'.join([save_path, added_path, f]),
-                                          allowlist='0123456789АаВвЕеКкМмНнОоРрСсТтУуХх',
-                                          detail=0, paragraph=True, min_size=60)[0])
-        except Exception as e:
-            result.append('')
-    return result
-
-
-# Функция, корректирующая распознанный номер
-def correct_plate_num(plate):
-    plate = plate.lower()  # все к нижнему регистру
-    # Из-за путаницы 0 и О явно указываем, где должны быть цифры, а где - буквы
-    part1 = plate[:1].replace('0', 'о')
-    part2 = plate[1:4].replace('о', '0')
-    part3 = plate[4:6].replace('0', 'о')
-    part4 = plate[6:].replace('o', '0')
-    return part1 + part2 + part3 + part4
-
-
-def correct(text):
-    text = text.lower()
-    dictionary = {
-        'a': 'а',
-        'b': 'в',
-        'e': 'е',
-        'k': 'к',
-        'm': 'м',
-        'h': 'н',
-        'o': 'о',
-        'p': 'р',
-        'c': 'с',
-        't': 'т',
-        'y': 'у',
-        'x': 'х'
-    }
-    for key in dictionary.keys():
-        text = text.replace(key, dictionary[key])
-    return text
+from model.utils import translator, predictor
 
 
 def test(model):
     df_output = pd.DataFrame(columns=['img_filename', 'ocr_output', 'ref_text', 'cer'])
-    mypath = '../../dataset/test'
-    files = [f for f in listdir(mypath) if not (f.endswith('.txt'))]  # список изображений
+    test_path = '../../dataset/test'
+    files = [f for f in listdir(test_path) if not (f.endswith('.txt'))]  # список тестовых изображений
 
     with open('../../dataset/train.json') as ann_f:
-        ann = json.load(ann_f)
+        ann = json.load(ann_f)  # файл с разметкой данных
         for img in files:
-            ref_txt = correct(list(filter(lambda x: x["file"] == "train/" + img, ann))[0]["nums"][0]["text"])
-            preds = predict(model, mypath + '/' + img)
-            corrected = '' if len(preds) == 0 else correct_plate_num(preds[0])
+            # Переводим правильный ответ с номером в русские символы
+            ref_txt = translator.translate(
+                list(filter(lambda x: x["file"] == "train/" + img, ann))[0]["nums"][0]["text"], translator.eng_ru)
+            # Получаем предсказания
+            preds = predictor.predict(model, test_path + '/' + img)
+            # Корректируем предсказания
+            corrected = '' if len(preds) == 0 else predictor.correct_plate_num(preds[0])
+            # Считаем CER
             cer = fastwer.score_sent(corrected, ref_txt, char_level=True)
+            # Заносим данные в датафрейм
             dictionary = {'img_filename': img, 'ocr_output': corrected, 'ref_text': ref_txt, 'cer': cer}
             df_output = df_output.append(dictionary, ignore_index=True)
     return df_output
@@ -91,7 +31,8 @@ def test(model):
 
 mdl = torch.hub.load('ultralytics/yolov5', 'custom', path='../../last.pt', force_reload=True, device='cpu')
 output = test(mdl)
-# Overall performances
-print(output.to_string())
+# Среднее CER
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(output.sort_values(by='cer', ascending=False))
 mean_cer = output['cer'].mean()
 print(f'Mean CER = {mean_cer}%')
